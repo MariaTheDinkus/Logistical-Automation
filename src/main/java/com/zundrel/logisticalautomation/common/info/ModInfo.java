531dbc716d8f34f6c19f2cefb8b2dedbf67c9463
Jacob Farley <jacobfarley2017@gmail.com>
2018-09-12 19:34:14 -0500

    Updated the version to the proper version.

--- src/main/java/com/zundrel/logisticalautomation/common/info/ModInfo.java
+++ src/main/java/com/zundrel/logisticalautomation/common/info/ModInfo.java
@@ -3,7 +3,7 @@ package com.zundrel.logisticalautomation.common.info;
 public class ModInfo {
 	public static final String MOD_NAME = "Logistical Automation";
 	public static final String MOD_ID = "logisticalautomation";
-	public static final String MOD_VERSION = "alpha1.2.1";
+	public static final String MOD_VERSION = "0.1.0";
 
 	public static final String MOD_CLIENT_PROXY = "com.zundrel.logisticalautomation.client.ClientProxy";
 	public static final String MOD_SERVER_PROXY = "com.zundrel.logisticalautomation.common.CommonProxy";
